2c842146cf61f9292dde2727d3f7c1692620fbf4
amrutaForGit <amruta.meetup@gmail.com>
2025-08-08 12:39:19 -0400

    Auto-generated: Add two numbers- Test2

---
+++ auto_add-two-numbers--test2.java
@@ -0,0 +1,10 @@
+//Java program to add two numbers
+public class NumberAddition {
+
+    //Method to add two numbers
+    public int addNumbers(int A, int B) {
+        // Adding two numbers A and B, storing result in C
+        int C = A + B;
+        return C;
+    }
+}
