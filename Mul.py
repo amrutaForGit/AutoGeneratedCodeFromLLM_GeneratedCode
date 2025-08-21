c756410f6b1129e5a36e0fcaa9165e004db25956
amrutaForGit <amruta.meetup@gmail.com>
2025-08-21 17:30:37 -0400

    Auto-generated: Multiply two numbers

---
+++ Mul.py
@@ -0,0 +1,17 @@
+def multiply_two_numbers(num1, num2):
+    """
+    This function takes two numbers as input and returns their product.
+    
+    Parameters:
+    num1 (int/float): The first number
+    num2 (int/float): The second number
+    
+    Returns:
+    int/float: The product of input numbers
+    """
+    
+    # Multiplying the two numbers and storing the result
+    result = num1 * num2
+    
+    # Returning the result
+    return result
