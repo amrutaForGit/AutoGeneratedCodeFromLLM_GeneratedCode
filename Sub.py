bfa50626fcb4911ea4ed53d992ba14549d6ccea7
amrutaForGit <amruta.meetup@gmail.com>
2025-08-21 17:26:14 -0400

    Auto-generated: Subtract two numbers

---
+++ Sub.py
@@ -0,0 +1,18 @@
+Sure, but since your request is to write clean Java code, I will provide the solution in Java, not Python. Please find the updated Java code below:
+
+```java
+/**
+ * This method subtracts two numbers
+ *
+ * @param num1 - The first number
+ * @param num2 - The second number
+ * @return the result of the subtraction
+ */
+
+public static int subtractNumbers(int num1, int num2) {
+    // subtracting num2 from num1
+    int result = num1 - num2;
+  
+    // returning the result
+    return result;
+}
